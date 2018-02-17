dfb632e420c6bb80a6ed17769ab4c8d586334d41
Alain NGUIDJOI BELL <alain.nguidjoi.bell@gmail.com>
2018-02-17 17:28:19 +0400

    looad

--- app/components/index.js
+++ app/components/index.js
@@ -2,27 +2,28 @@
 
 import allcss from '../index.js';
 import angular from 'angular';
-import { name as ngstorage } from 'ngstorage';
+//import { name as ngstorage } from 'ngstorage';
 import angular_ui_router from 'angular-ui-router';
-import ngCacheBuster from "angular-cache-buster";
+//import ngCacheBuster from "angular-cache-buster";
 import account from './account'
 import authentication from './authentication';
 import bankInformation from './bankInformation';
 import homes from './home';
 import personnalInformation from './personnalInformation';
-import httpConfig from './config/http-config';
-import localStorageConfig from './config/local-storage-config'
-import authInterceptor from './interceptor/auth-interceptor';
-import autExpiredInterceptor from './interceptor/auth-expired-interceptor';
-import errorHandlerInterceptor from './interceptor/error-handler-interceptor';
-import notificationInterceptor from './interceptor/notofication-interceptor';
+//import httpConfig from './config/http-config';
+//import localStorageConfig from './config/local-storage-config'
+//import authInterceptor from './interceptor/auth-interceptor';
+//import autExpiredInterceptor from './interceptor/auth-expired-interceptor';
+//import errorHandlerInterceptor from './interceptor/error-handler-interceptor';
+//import notificationInterceptor from './interceptor/notofication-interceptor';
 
 
 angular
     .module('ae', [
         // external libs
+        //ngstorage,
         angular_ui_router,
-        ngCacheBuster,
+        //ngCacheBuster,
         // internal module
         account.name,
         authentication.name,
