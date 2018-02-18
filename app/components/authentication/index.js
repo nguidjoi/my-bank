5d4571fadce977f439219b755e578ab8d85c3b54
Alain NGUIDJOI BELL <alain.nguidjoi.bell@gmail.com>
2018-02-18 22:50:56 +0400

    load

--- app/components/authentication/index.js
+++ app/components/authentication/index.js
@@ -1,6 +1,6 @@
 'use strict';
 import authenticationCtrl from './authentication-controller';
-import authenticationSrv from './authentication-service';
+import auth from './authentication-service';
 import { authServerProvider } from './authentication-provider'
 import authConfig from './routes';
 export default angular
