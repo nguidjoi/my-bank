f800930d534f9613bf289d18b0c1cded3f933337
Alain NGUIDJOI BELL <alain.nguidjoi.bell@gmail.com>
2018-02-18 22:42:00 +0400

    load

--- app/components/account/index.js
+++ app/components/account/index.js
@@ -1,6 +1,6 @@
 'use strict';
 import accountCtrl from './account-controller';
-import account from './account-service';
+import { account } from './account-service';
 import accountConfig from './routes';
 
 export default angular
