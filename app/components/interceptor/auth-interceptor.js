b8603faf26870e0a48a560c2f8f7afcbad865fbb
Alain NGUIDJOI BELL <alain.nguidjoi.bell@gmail.com>
2018-02-24 21:59:36 +0400

    add

--- app/components/interceptor/auth-interceptor.js
+++ app/components/interceptor/auth-interceptor.js
@@ -10,11 +10,12 @@ export function authInterceptor($rootScope, $q, $location, $localStorage, $sessi
 
     function request(config) {
         if (!config || !config.url || /^http/.test(config.url)) return config;
-
+        console.log('authInterceptor');
         /*jshint camelcase: false */
         config.headers = config.headers || {};
         var token = $localStorage.authenticationToken || $sessionStorage.authenticationToken;
         if (token) {
+            console.log('authInterceptor find token');
             config.headers.Authorization = 'Bearer ' + token;
         }
 
